c600252d4a7ceaa95bf76b31ec66f7c799b84dfc
student <student@PATC19.local>
2020-02-26 10:53:51 -0800

    Creating Custom JavaScript Functions

---
+++ practice/javascript/week_6/functions.js
@@ -0,0 +1,43 @@
+// Intro to Functions
+
+// Example of functions we have used so far.
+console.log();
+
+Math.round();
+Math.floor();
+Math.ceil();
+Math.random();
+
+var currentTime = Date.now();
+console.log(currentTime);
+
+// A function that belongs to a class ("library") is called a "Method".
+// A variable that belongs to a class ("library") is called a "Property".
+
+
+
+// The Syntax Structure of a Function definition.
+function nameOfFunction() {
+    console.log("Hello");
+    console.log("Hello");
+    console.log("Hello");
+}
+
+
+nameOfFunction();
+nameOfFunction();
+
+
+
+/*
+Similar to CSS Rulesets
+
+selector {
+    property: value;
+    property: value;
+}
+
+*/
+
+
+nameOfFunction();
