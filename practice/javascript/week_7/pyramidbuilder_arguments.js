e79f836eaf49b6207415adda4b5e35f29c3515e3
student <student@PATC19.local>
2020-03-05 10:56:54 -0800

    Process.argv notes

--- practice/javascript/week_7/pyramidbuilder_arguments.js
+++ practice/javascript/week_7/pyramidbuilder_arguments.js
@@ -36,17 +36,21 @@ function pyramidBuilder(height) {
 }
 
 
-
+// Gets an array built by process.argv and stores in arguments variable.
 var arguments = process.argv;
 console.log(arguments);
 
+// We grab the 3rd item from the arguments array and store it in height.
 var height = arguments[2];
+
+// We grab the 4th item from arguments array and convert it to a number and then store in loops.
 var loops = parseInt( arguments[3] );
 
+// Take value of height, convert into a number, put it back into height.
 height = parseInt(height);
 console.log(height);
 
-
+// We loop for the amount of loops, and run pyramidBuilder with the approriate height value.
 for (var i = 0; i < loops; i++) {
     pyramidBuilder(height);
 }
